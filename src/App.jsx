3861b1e8f4c1da8e7fe570364acd1ec2369e3728
Sandeep Kumar Gupta <sandeep.g@redroad.in>
2024-12-13 16:03:03 +0530

    added auth and routes for all pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,67 @@
 import './App.css'
-import Navbar from '@/Components/Header/Navbar'
-import PageSidebar from '@/Components/Sidebar/PageSidebar'
-import Login from './Pages/Login'
-import {
-    SidebarInset,
-    SidebarProvider,
-} from '@/components/ui/sidebar'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Billing from '@/Pages/Billing/Listing/Billing'
+import Login from '@/Pages/Login'
+import Dashboard from '@/Pages/Dashboard/Dashboard'
+import AuthLayout from '@/Components/AuthLayout'
+import Departments from '@/Pages/Department/Listing/Departments'
+import Clients from '@/Pages/Clients/Listing/Clients'
+import Process from '@/Pages/Process/Listing/Process'
+import ProjectMaster from '@/Pages/ProjectMaster/ProjectMaster'
+import Invoice from '@/Pages/Invoice/Listing/Invoice'
+import DailyWork from '@/Pages/DailyWork/DailyWork'
+import { AuthProvider } from '@/Context/AuthContext'
 
-
+let router = createBrowserRouter([
+    {
+        path: '/login',
+        Component: Login,
+    },
+    {
+        path: '/',
+        Component: AuthLayout,
+        children: [
+            {
+                path: '/',
+                Component: Dashboard,
+            },
+            {
+                path: '/invoices',
+                Component: Invoice,
+            },
+            {
+                path: '/daily-work',
+                Component: DailyWork,
+            },
+            {
+                path: '/master-settings/billing',
+                Component: Billing,
+            },
+            {
+                path: '/master-settings/departments',
+                Component: Departments,
+            },
+            {
+                path: '/master-settings/clients',
+                Component: Clients,
+            },
+            {
+                path: '/master-settings/processes',
+                Component: Process,
+            },
+            {
+                path: '/master-settings/project-master',
+                Component: ProjectMaster,
+            },
+        ],
+    },
+])
 
 function App() {
-    
     return (
-        <>
-            <SidebarProvider>
-                <PageSidebar />
-                <SidebarInset >
-                    <Navbar />
-                    <div className="flex flex-1 flex-col gap-4 p-4">
-                        <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-                            <div className="aspect-video rounded-xl bg-red-200" />
-                            <div className="aspect-video rounded-xl bg-red-200" />
-                            <div className="aspect-video rounded-xl bg-red-200" />
-                        </div>
-                        <div className="min-h-[100vh] flex-1 rounded-xl bg-red-200 md:min-h-min" />
-                    </div>
-                </SidebarInset>
-            </SidebarProvider>
-            {/* <div className="h-[calc(100vh-3.5rem)]">
-                <Login />
-            </div> */}
-        </>
-        // <Login />
+        <AuthProvider>
+            <RouterProvider router={router} />
+        </AuthProvider>
     )
 }
 
